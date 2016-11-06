3dce5751e34edd583a20e82f0085c995dc6f3b0b
KhalladB <eng.khallad@hotmail.com>
2016-11-06 12:47:38 +0300

    Update openag_dfr0300.h

--- openag_dfr0300.h
+++ openag_dfr0300.h
@@ -44,15 +44,15 @@
   float _ec_calibration_offset;
   
   //
-  OneWire _w_oneWire;
-  const int w_pin = 5;
-  DeviceAddress _w_address;
-  DallasTemperature _w_sensors;
+  //OneWire _w_oneWire;
+  //const int w_pin = 5;
+  //DeviceAddress _w_address;
+  //DallasTemperature _w_sensors;
   //bool _send_water_temperature;
-  float _w_water_temperature;
-  uint32_t _w_time_of_last_query;
-  bool _w_waiting_for_conversion;
-  const static uint32_t _w_min_update_interval = 1800;
+  //float _w_water_temperature;
+  //uint32_t _w_time_of_last_query;
+  //bool _w_waiting_for_conversion;
+  //const static uint32_t _w_min_update_interval = 1800;
   //int _w_pin = 5;
   //int w_pin = _w_pin;
 
