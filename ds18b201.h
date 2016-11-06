41a7e94bb5c622190b99330e04255b14756fef49
KhalladB <eng.khallad@hotmail.com>
2016-11-06 12:47:32 +0300

    Update and rename ds18b20.h to ds18b201.h

--- ds18b201.h
+++ ds18b201.h
@@ -14,20 +14,22 @@
  */
 class Ds18b20 : public Module {
   public:
-    Ds18b20(int _w_pin=5);
+    Ds18b20(int _w_pin);
     void begin();
     //void update();
     // bool get_water_temperature(std_msgs::Float32 &msg);
 
   private:
-    OneWire _w_oneWire;
+    OneWire _oneWireW;
+    const int _w_pin = 5;
+    w_pin = _w_pin;
     DeviceAddress _w_address;
     DallasTemperature _w_sensors;
     //bool _send_water_temperature;
     float _w_water_temperature;
     uint32_t _w_time_of_last_query;
     bool _w_waiting_for_conversion;
-    const static uint32_t _w_min_update_interval = 2000;
+    const static uint32_t _w_min_update_interval = 1800;
 };
 
 #endif
