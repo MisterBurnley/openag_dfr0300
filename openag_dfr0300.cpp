0000f5f8f9208795474a2016bb954ee298b2db38
KhalladB <eng.khallad@hotmail.com>
2016-11-06 12:18:07 +0300

    Update openag_dfr0300.cpp

--- openag_dfr0300.cpp
+++ openag_dfr0300.cpp
@@ -1,8 +1,9 @@
 
  #include "openag_dfr0300.h"
+ #include <OneWire.h>
  //#include "ds18b20.h"
  
- Dfr0300::Dfr0300(int ec_pin):{
+ Dfr0300::Dfr0300(int ec_pin){
    OneWire _w_oneWire=5;
    _w_sensors = DallasTemperature(&_w_oneWire);
    _w_sensors.setWaitForConversion(false);
