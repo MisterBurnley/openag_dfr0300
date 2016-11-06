0419d6ab52bb71ee7b2d9f2b8b530455a73e221d
KhalladB <eng.khallad@hotmail.com>
2016-11-06 14:47:28 +0300

    Update openag_dfr0300.h

--- openag_dfr0300.h
+++ openag_dfr0300.h
@@ -38,7 +38,7 @@
   float _water_electrical_conductivity;
   bool _send_water_electrical_conductivity;
   uint32_t _time_of_last_query;
-  const uint32_t _min_update_interval = 2000;
+  const uint32_t _min_update_interval = 1800;
   
   float _ec_calibration_coefficient;
   float _ec_calibration_offset;
